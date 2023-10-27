45a01027aa7ead54ff2ccdf6b03c657679cd37fb
indushika <indushikaks@gmail.com>
2023-10-28 00:00:32 +0530

    Clean up

--- Assets/Scripts/Infrastructure/Systems/GameLifetimeScope.cs
+++ Assets/Scripts/Infrastructure/Systems/GameLifetimeScope.cs
@@ -2,10 +2,8 @@ using VContainer;
 using VContainer.Unity;
 using MessagePipe;
 using MonsterFactory.Services;
-using MonsterFactory.Services.DataManagement;
 using MonsterFactory.Services.SceneManagement;
 using MonsterFactory.Events;
-using MonsterFactory.Services.Session;
 using UnityEngine;
 
 public class GameLifetimeScope : LifetimeScope
@@ -15,34 +13,21 @@ public class GameLifetimeScope : LifetimeScope
     protected override void Configure(IContainerBuilder builder)
     {
         SetupGlobalMessageBrokers(builder);
-        SetupSession(builder);
         SetupServices(builder);
     }
 
     private void SetupServices(IContainerBuilder builder)
     {
-        builder.Register<DataManager>(Lifetime.Scoped).AsImplementedInterfaces().AsSelf();
-        //builder.RegisterEntryPoint<MFService>(Lifetime.Transient);
-        builder.Register<DataInstanceProvider>(Lifetime.Singleton);
-        builder.Register<DataConnector>(Lifetime.Singleton).As<IGenericConnector>();
         builder.RegisterInstance(_sceneTransitionController).AsImplementedInterfaces();
         builder.Register<SceneNavigationManager>(Lifetime.Singleton).AsImplementedInterfaces();
-
-        builder.RegisterEntryPoint<GameInitializer>();
     }
-
-    private void SetupSession(IContainerBuilder builder)
-    {
-        SessionManager.CreateSession();
-    }
+    
 
     private void SetupGlobalMessageBrokers(IContainerBuilder builder)
     {
         MessagePipeOptions options = builder.RegisterMessagePipe();
-        //Setup GlobalMessagePipe to enable diagnostics window and global function 
         builder.RegisterBuildCallback(c => GlobalMessagePipe.SetProvider(c.AsServiceProvider()));
         
-        builder.RegisterMessageBroker<MFInternalServicesEvent>(options);
         EventRegistrationHelper.RegisterEventClasses(builder,options);
     }
     
